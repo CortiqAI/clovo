11e5e95d54fca37bb582c20daea54c55caf4f3f2
CortiqAI <Shahnawaz101125@gmail.com>
2025-02-18 17:09:08 -0500

    Create clovo-did.js

---
+++ clovo-did.js
@@ -0,0 +1,11 @@
+(function() {
+    var script = document.createElement('script');
+    script.type = "module";
+    script.src = "https://agent.d-id.com/v1/index.js";
+    script.setAttribute("data-name", "did-agent");
+    script.setAttribute("data-mode", "fabio");
+    script.setAttribute("data-client-key", "Z29vZ2xlLW9hdXRoMnwxMTc4OTI4MzQyMTM1NDc0NTk0MTg6UHA1am5handQT3BMRGZucjZJa1Rn");
+    script.setAttribute("data-agent-id", "agt_NDtgoMwe");
+    script.setAttribute("data-monitor", "true");
+    document.body.appendChild(script);
+})();
